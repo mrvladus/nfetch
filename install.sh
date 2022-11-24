17a0174105ceaffa72764860661cafb1742bd0bf
mrvladus <64690334+mrvladus@users.noreply.github.com>
2022-11-24 12:51:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing nfetch..."
 
-sudo cp ./nfetch /usr/local/bin/nfetch
+wget -qO- https://raw.githubusercontent.com/mrvladus/nfetch/master/nfetch | sudo tee /usr/local/bin/nfetch
 sudo chmod +x /usr/local/bin/nfetch
 
 echo "Installation done! You can run 'nfetch' now."
